880cf07762405a5ed2c7cd00906039ea8406f89b
Thoriqaafif <thoriq.afif.habibi@gmail.com>
2023-11-13 07:03:56 +0700

    inital chat room using python socket

---
+++ server.py
@@ -0,0 +1,61 @@
+import socket
+import sys
+from _thread import *
+
+clients = list()
+messageSize = 1024
+
+def remove(connection):
+    connection.close()
+    if(connection in clients):
+        clients.remove(connection)
+
+def broadcast(message, sender):
+    for client in clients:
+        if(client != sender):
+            try:
+                client.send(message)
+            except:
+                remove(client)
+
+def clientConnection(conn, addr):
+    while True:
+        try:
+            message = conn.receive(messageSize)
+            if message:
+                print(f"Message: {message}")
+                print(f"Sender: {addr[0]}")
+
+                broadcast(f"{addr[0]},{message}", conn)
+            
+            else:
+                remove(conn)
+        except:
+            continue
+
+if __name__ == '__main__':
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+    # sufficient argument is not provided
+    if(len(sys.argv) != 3):
+        print ("Jalankan dengan memberi IP dan PORT: python server.py IP PORT")
+        exit(1)
+    
+    IP = sys.argv[1]
+    PORT = int(sys.argv[2])
+
+    server.bind((IP, PORT))
+    server.listen(50)
+
+    # server accept connection request from each client
+    while True:
+        conn, addr = server.accept()
+        clients.append(conn)
+
+        print(f"{addr[0]} is Connected")
+
+        start_new_thread(clientConnection, (conn, addr))
+
+    conn.close()
+    server.close()
